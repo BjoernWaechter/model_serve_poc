ff30b2b1e4ad0c912abed89ee11799b800187b99
Björn Wächter <bjoern@waechterei.de>
2026-04-12 09:56:22 +1200

    gpu nodes enabled with automatic scale up

--- eks-ray/iac/eks.tf
+++ eks-ray/iac/eks.tf
@@ -41,25 +41,67 @@ module "eks" {
 
  eks_managed_node_groups = {
    rayserve-cpu = {
-     partition = "aws"
-     instance_types = ["t3.xlarge"]
-     min_size       = 0
-     max_size       = 5
-     desired_size   = 1
-     iam_role_additional_policies = {
+      partition = "aws"
+      instance_types = ["t3.xlarge"]
+      min_size       = 0
+      max_size       = 5
+      desired_size   = 1
+      iam_role_additional_policies = {
         AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
         AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
       }
+      tags = {
+        "k8s.io/cluster-autoscaler/enabled" = "true"
+        "k8s.io/cluster-autoscaler/${module.eks.cluster_name}" = "owned"
+      }
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size = 100
+            volume_type = "gp3"
+            encrypted   = true
+            delete_on_termination = true
+          }
+        }
+      }
+      metadata_options = {
+        http_endpoint               = "enabled"
+        http_put_response_hop_limit = 2
+        http_tokens                 = "required"
+      }
+      
    }
    rayserve-gpu = {
-     partition = "aws"
-     instance_types = ["g4dn.xlarge"]
-     min_size       = 0
-     max_size       = 5
-     desired_size   = 0
-     iam_role_additional_policies = {
+      partition = "aws"
+      instance_types = ["g4dn.xlarge"]
+      ami_type       = "AL2023_x86_64_NVIDIA"
+      min_size       = 0
+      max_size       = 5
+      desired_size   = 0
+      iam_role_additional_policies = {
         AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
         AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+      }
+      tags = {
+        "k8s.io/cluster-autoscaler/enabled" = "true"
+        "k8s.io/cluster-autoscaler/${module.eks.cluster_name}" = "owned"
+      }
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size = 100
+            volume_type = "gp3"
+            encrypted   = true
+            delete_on_termination = true
+          }
+        }
+      }
+      metadata_options = {
+        http_endpoint               = "enabled"
+        http_put_response_hop_limit = 2
+        http_tokens                 = "required"
       }
    }
  }
@@ -103,10 +145,91 @@ module "eks_blueprints_addons" {
 
 }
 
+module "cluster_autoscaler" {
+  source  = "lablabs/eks-cluster-autoscaler/aws"
+  version = "4.0.0"
+
+  cluster_name                     = module.eks.cluster_name
+  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  irsa_role_name_prefix            = "rayserver-irsa"
+
+  depends_on = [
+    module.eks
+  ]
+}
 
 resource "time_sleep" "wait_k8s_ready" {
   depends_on = [module.eks_blueprints_addons]
 
   create_duration  = "30s"
   destroy_duration = "30s"
+}
+
+
+resource "helm_release" "nvidia_device_plugin" {
+  name = "nvidia-device-plugin"
+
+  repository = "https://nvidia.github.io/k8s-device-plugin"
+  chart      = "nvidia-device-plugin"
+  namespace  = "kube-addons"
+  create_namespace = "true"
+
+  depends_on = [
+    module.eks.eks_managed_node_groups  # or your node group resource
+  ]
+
+  set {
+    name = "version"
+    value = "0.19.0"
+  }
+  set {
+    name  = "tolerations[0].key"
+    value = "nvidia.com/gpu"
+  }
+
+  set {
+    name  = "tolerations[0].operator"
+    value = "Exists"
+  }
+
+  set {
+    name  = "tolerations[0].effect"
+    value = "NoSchedule"
+  }
+}
+
+
+
+resource "aws_iam_policy" "s3_ray_artifact_bucket" {
+  name        = "s3-ray-artifact-bucket"
+  description = "Allow read-only access to a specific S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::ray-ap-southeast-2",
+          "arn:aws:s3:::ray-ap-southeast-2/*"
+        ]
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "cpu_nodes_s3_read" {
+  role       = module.eks.eks_managed_node_groups["rayserve-cpu"].iam_role_name
+  policy_arn = aws_iam_policy.s3_ray_artifact_bucket.arn
+}
+
+resource "aws_iam_role_policy_attachment" "gpu_nodes_s3_read" {
+  role       = module.eks.eks_managed_node_groups["rayserve-gpu"].iam_role_name
+  policy_arn = aws_iam_policy.s3_ray_artifact_bucket.arn
 }
---
+++ eks-ray/kuberay/text_summarizer_req.py
@@ -0,0 +1,39 @@
+import requests
+
+# Deploy service : serve run text_summarizer:deployment
+
+text = """This article is about the bird. For the band, see Emperor Penguin (band).
+Emperor penguin
+Adults with a chick on Snow Hill Island, Antarctic Peninsula
+Conservation status
+Endangered
+Endangered (IUCN 3.1)[1]
+Scientific classification Edit this classification
+Kingdom: 	Animalia
+Phylum: 	Chordata
+Class: 	Aves
+Order: 	Sphenisciformes
+Family: 	Spheniscidae
+Genus: 	Aptenodytes
+Species: 	A. forsteri
+Binomial name
+Aptenodytes forsteri
+Gray, 1844
+
+The emperor penguin (Aptenodytes forsteri) is the tallest and heaviest of all living penguin species and is endemic to Antarctica. The male and female are similar in plumage and size, reaching 100 cm (39 in) in length and weighing from 22 to 45 kg (49 to 99 lb). Feathers of the head and back are black and sharply delineated from the white belly, pale-yellow breast and bright-yellow ear patches.
+
+Like all species of penguin, the emperor is flightless, with a streamlined body, and wings stiffened and flattened into flippers for a marine habitat. Its diet consists primarily of fish, but also includes crustaceans, such as krill, and cephalopods, such as squid. While hunting, the species can remain submerged around 20 minutes, diving to a depth of 535 m (1,755 ft). It has several adaptations to facilitate this, including an unusually structured haemoglobin to allow it to function at low oxygen levels, solid bones that reduce barotrauma, and the ability to reduce its metabolism and shut down non-essential organ functions.
+
+The only penguin species that breeds during the Antarctic winter, emperor penguins trek 50-120 km (31-75 mi) over the ice to breeding colonies which can contain up to several thousand individuals. The female lays a single egg, which is incubated for just over two months by the male while the female returns to the sea to feed; parents subsequently take turns foraging at sea and caring for their chick in the colony. The lifespan of an emperor penguin is typically 20 years in the wild, although observations suggest that some individuals may live as long as 50 years of age.
+Taxonomy
+
+Emperor penguins were described in 1844 by English zoologist George Robert Gray, who created the generic name from Ancient Greek word elements, ἀ-πτηνο-δύτης [a-ptēno-dytēs], "without-wings-diver". Its specific name is in honour of the German naturalist Johann Reinhold Forster, who accompanied Captain James Cook on his second voyage and officially named five other penguin species.[2] Forster may have been the first person to see emperor penguins in 1773-74, when he recorded a sighting of what he believed was the similar king penguin (A. patagonicus) but, given the location, may very well have been the emperor penguin (A. forsteri).[3]
+
+Together with the king penguin, the emperor penguin is one of two extant species in the genus Aptenodytes. Fossil evidence of a third species—Ridgen's penguin (A. ridgeni)—has been found from the late Pliocene, about three million years ago, in New Zealand.[4] Studies of penguin behaviour and genetics have proposed that the genus Aptenodytes is basal; in other words, that it split off from a branch which led to all other living penguin species.[5] Mitochondrial and nuclear DNA evidence suggests this split occurred around 40 million years ago.[6]
+Description
+Adults with chicks
+
+"""
+print(text)
+res = requests.get("http://<loadbalancer>:8000/summarize", params={"text": text})
+print(res.json())
